eb00e78d4f4aa5d6b99b1ca781a781c452fd44aa
snehasusansaji <62944723+snehasusansaji@users.noreply.github.com>
2020-03-31 20:17:51 +0530

    Create details.c

---
+++ details.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+  
+char name[]="SNEHA SUSAN SAJI";
+char branch[]="ELECTRICAL AND ELECTRONICS";
+char college []="COLLEGE OF ENGINEERING CHENGANNUR";
+  printf("%s",&name);
+  
+  printf("\n%s",&branch);
+  
+  printf("\n%s",& college);
+}
